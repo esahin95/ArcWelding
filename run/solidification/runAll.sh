6f5da43ac3d09efc0c28da65636d5fc43c8440d6
esahin95 <emre.sahin95@web.de>
2025-09-04 09:21:52 +0200

    added test case for solidification

---
+++ run/solidification/runAll.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# clean case directory
+foamCleanCase
+rm -r 0 >/dev/null 2>&1
+cp -r 0_orig 0
+
+# create new mesh
+blockMesh > log.blockMesh
+createPatch -overwrite > log.createPatch
+checkMesh > log.checkMesh
+
+# initialize fields
+setFields > log.setFields
+
+# run solver
+decomposePar -cellDist > log.decompose
+mpirun -np 4 interThermoFoam -parallel > log.solver
+reconstructPar > log.reconstruct
